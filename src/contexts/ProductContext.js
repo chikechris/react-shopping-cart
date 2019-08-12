ccd8074f49ae5b8f0f01b5022c67d9fc40435beb
Chike Chris <cchike2017@gmail.com>
2019-08-12 15:10:00 -0400

    provided data with ProductContext

---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,10 @@
+import React, { createContext } from "react";
+
+const ProductContext = createContext();
+console.log(ProductContext);
+
+const ProductContext = () => {
+  return <div />;
+};
+
+export default ProductContext;
